44b4fa5934c69f5c78f41b67ac68f6f2760f6414
Benny <bennypythoncoder@gmail.com>
2022-09-12 20:59:53 -0400

    Create install.py

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+
+print("[...] Installing requirements tqdm, colorama...")
+os.system("pip install tqdm colorama")
+print("""\n
+[✓] Installation should be complete.
+Run the Collatz Cruncher program using:
+python3 CollatzCruncher.py
+""")
